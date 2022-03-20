9e7c2b0a6e49ccedd0e0f24a2d6989e8195a15ea
codeqod <51000626+codeqod@users.noreply.github.com>
2022-03-20 22:11:29 +0100

    Add Update, Insert, Delect and Select to MongoDatabase

--- src/main/java/de/minefinity/global/objects/MFTable.java
+++ src/main/java/de/minefinity/global/objects/MFTable.java
@@ -1,17 +1,10 @@
 package de.minefinity.global.objects;
 
+import com.mongodb.client.MongoDatabase;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.annotation.Nullable;
-import javax.xml.transform.Result;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 
 public class MFTable {
 
@@ -23,51 +16,33 @@ public class MFTable {
      *
      */
 
-    public String table = "";
+    public String collection = "";
 
     @Getter @Setter
-    public MFDatabase database;
+    public MongoDatabase mongoDatabase;
 
 
 
 
-    public MFTable(String TABLE) {
-        this.table = TABLE;
+    public MFTable(String collection, MFDatabase mfDatabase) {
+        this.collection = collection;
+
+        this.setMongoDatabase(mfDatabase.getMongoDatabase());
+    }
+
+    public void insert(Document document){
+        this.getMongoDatabase().getCollection(this.collection).insertOne(document);
     }
 
 
-    public ResultSet select(HashMap<String, String> where){
-
-        ArrayList<String> whereArray = new ArrayList<>();
-        ArrayList<String> whereValues = new ArrayList<>();
-
-
-        for(Map.Entry<String, String> entries : where.entrySet()){
-            whereArray.add(entries.getKey() + " = ? ");
-            whereValues.add(entries.getValue());
-        }
-
-        String sql = "SELECT * FROM " + table + (where.size() >= 1 ? " WHERE " + String.join(" AND ", whereArray) : "");
-
-        ResultSet resultSet = null;
-
-        try {
-            PreparedStatement preparedStatement = this.getDatabase().getConnection().prepareStatement(sql);
-
-            for(int i = 0; i <= whereValues.size(); i++){
-                preparedStatement.setString(i, whereValues.get(i));
-            }
-
-            resultSet = preparedStatement.executeQuery();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return resultSet;
-
+    public void delete(Bson filter){
+        this.getMongoDatabase().getCollection(this.collection).deleteMany(filter);
     }
 
+
+    public void update(Bson filter, Document document){
+        this.getMongoDatabase().getCollection(this.collection).updateOne(filter, document);
+    }
 
 
 
